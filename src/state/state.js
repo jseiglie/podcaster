5ed2fcee13ef04eab6b7b9cd8e4b6e836a9d1e65
Javier Seiglie <javierseiglie@gmail.com>
2023-06-28 12:53:12 +0200

    feat(data validity): Added feat to only fetch new latest podcast if it has passed more than one day

--- src/state/state.js
+++ src/state/state.js
@@ -8,6 +8,26 @@ const getState = ({ getStore, getActions, setStore }) => {
       episodeDetails: null,
     },
     actions: {
+      checkValidity: () => {
+        const now = new Date()
+          .toLocaleDateString()
+          .split("/")
+          .reverse()
+          .join("/");
+        
+        if (localStorage.getItem("validity")) {
+          const result =
+            new Date(localStorage.getItem("validity")).getTime() -
+            new Date(now).getTime();
+          if (Math.round(result / (1000 * 60 * 60 * 24)) !== 0) {
+            //returns false if it has been more than 24 hours since last fetch and needs to fetch
+            return false;
+          }
+          //returns true if it doesn't needs to fetch new data
+          setStore({ latest: JSON.parse(localStorage.getItem("latest"))});
+          return true;
+        }
+      },
       getLatestPodcast: async () => {
         const resp = await fetch(
           "https://itunes.apple.com/us/rss/toppodcasts/limit=100/genre=1310/json"
@@ -15,6 +35,10 @@ const getState = ({ getStore, getActions, setStore }) => {
         const data = await resp.json();
         setStore({ latest: data.feed });
         localStorage.setItem("latest", JSON.stringify(data.feed));
+        localStorage.setItem(
+          "validity",
+          new Date().toLocaleDateString().split("/").reverse().join("/")
+        );
       },
       getPodcastEpisodes: async (id) => {
         const resp = await fetch(
@@ -37,16 +61,15 @@ const getState = ({ getStore, getActions, setStore }) => {
           );
       },
       setEpisodeDetails: (ep) => {
-        console.log(ep)
-        setStore({ episodeDetails: ep })
-        
+        console.log(ep);
+        setStore({ episodeDetails: ep });
       },
       getFeed: async (rss) => {
         //console.log("RSS", rss);
 
         const resp = await fetch("https://jbpod.libsyn.com/applepodcast");
         const { contents } = await resp.text();
-       // console.log(await contents);
+        // console.log(await contents);
         const feed = new window.DOMParser().parseFromString(
           contents,
           "text/xml"
--- src/views/Home.js
+++ src/views/Home.js
@@ -6,20 +6,25 @@ export const Home = () => {
   const { store, actions } = useContext(Context);
 
   useEffect(() => {
-    actions.getLatestPodcast();
+    if (!actions.checkValidity()) actions.getLatestPodcast();
   }, []);
 
   const handleSearch = (e) => {};
 
   return (
     <article>
-       <section className="navbar">
+      <section className="navbar">
         <span className="counter">
           {JSON.parse(localStorage.getItem("latest"))
             ? JSON.parse(localStorage.getItem("latest")).entry.length
             : ""}
         </span>
-        <input className="search" type="text" placeholder="Filter podcasts..." onChange={(e) => handleSearch(e)} />
+        <input
+          className="search"
+          type="text"
+          placeholder="Filter podcasts..."
+          onChange={(e) => handleSearch(e)}
+        />
       </section>
       <PodcastListComponent />
     </article>
